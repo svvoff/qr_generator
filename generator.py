f4205ee19dc728472ce451da6a12ebc1960b96e9
Andrei Sorokin <andrey.s.sorokin@raiffeisen.ru>
2019-06-23 17:34:35 +0300

    save image with background;

--- generator.py
+++ generator.py
@@ -53,20 +53,32 @@ def create_qr(data):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=4,
+        box_size=5,
         border=2,
     )
 
     qr.add_data(data)
     qr.make(fit=True)
 
-    return qr.make_image(image_factory=ImageFactory, fill_color="black", back_color="white")
+    return qr.make_image(image_factory=ImageFactory, fill_color="black", back_color="#C1BAAE")
 
-
+angle = 0
 for i in range(0, 10):
+    from os import listdir
+    from os.path import isfile, join
+
+    onlyfiles = [f for f in listdir("backgrounds") if isfile(join("backgrounds", f))]
+
     text = textProvider.get_text()
     img = create_qr(text)
-    angle = random.randrange(0, 360)
     img = img.rotate(angle, expand=1, fillcolor=None)
-    img.save("./output/" + str(i) + ".png")
+    qrsize = img.size
+    background = Image.open("backgrounds/01.jpg", "r").convert('RGBA')
+    backgroundsize = background.size
+    xbound = backgroundsize[0] - qrsize[0]
+    ybound = backgroundsize[1] - qrsize[1]
+    dest = (random.randrange(0, xbound), random.randrange(0, ybound))
+    background.alpha_composite(img, dest=dest)
+    background.convert('RGB').save("./output/" + str(i) + ".jpg", format="JPEG")
+    angle += 10
 
