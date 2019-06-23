9563d395d8da888402846360dfe25cfea7b9868e
Andrei Sorokin <andrey.s.sorokin@raiffeisen.ru>
2019-06-23 13:35:43 +0300

    generating qr with transparent color;

--- generator.py
+++ generator.py
@@ -1,17 +1,72 @@
 import qrcode
+import random
+from PIL import Image, ImageDraw
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=2,
-    border=1,
-)
+class ImageFactory(qrcode.image.base.BaseImage):
+    """
+    PIL image builder, default format is PNG.
+    """
+    kind = "PNG"
 
-qr.add_data('''
-ST00012|Name=ДЕПАРТАМЕНТ ФИНАНСОВ ГОРОДА МОСКВЫ (ГБОУ Школа имени Маяковского л/с 2607542000900468)|PersonalAcc=40601810245253000002|BankName=ГУ БАНКА РОССИИ ПО ЦФО Г. МОСКВА 35|BIC=044525000|CorrespAcc=0|Sum=250000|Purpose=СНИЛС:ХХХХХХ03307.Оплата за Калинин Никита Васильевич (ЛС 09020),Дог-р №1069-ДО, 2018-19 уч.год Развивающий курс "Математика +" Горская И.Ф., Октябрь.2018, НДС не обл.|PayeeINN=7723103106|DrawerStatus=24|KPP=772301001|CBC=07500000000000000137|OKTMO=45396000|lastName=Калинина|firstName=Светлана|middleName=Васильевна|payerAddress=109044, Москва г, Дубровская 1-я ул, дом № 2а, кв.18|contract=1069-ДО|persAcc=09020|childFio=Калинин Никита Васильевич|uin=0349347501810000000132623
-''')
-qr.make(fit=True)
+    def new_image(self, **kwargs):
+        back_color = kwargs.get("back_color", "white")
+        fill_color = kwargs.get("fill_color", "black")
 
-img = qr.make_image(fill_color="black", back_color="white")
+        img = Image.new("RGBA", (self.pixel_size, self.pixel_size), back_color)
+        self.fill_color = fill_color
+        self._idr = ImageDraw.Draw(img)
+        return img
 
-img.save("generated.png")
+    def drawrect(self, row, col):
+        box = self.pixel_box(row, col)
+        self._idr.rectangle(box, fill=self.fill_color)
+
+    def save(self, stream, format=None, **kwargs):
+        if format is None:
+            format = kwargs.get("kind", self.kind)
+        if "kind" in kwargs:
+            del kwargs["kind"]
+        self._img.save(stream, format=format, **kwargs)
+
+    def __getattr__(self, name):
+        return getattr(self._img, name)
+
+
+class TextProvider:
+    read = 0
+
+    textFile = open("war.txt", "r")
+    text = textFile.read()
+    textFile.close()
+
+    def get_text(self):
+        length = random.randrange(20, 800)  # 20 -> 800
+        qr_string = self.text[self.read: length + self.read]
+        self.read += length
+        return qr_string
+
+
+textProvider = TextProvider()
+
+
+def create_qr(data):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=4,
+        border=2,
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    return qr.make_image(image_factory=ImageFactory, fill_color="black", back_color="white")
+
+
+for i in range(0, 10):
+    text = textProvider.get_text()
+    img = create_qr(text)
+    angle = random.randrange(0, 360)
+    img = img.rotate(angle, expand=1, fillcolor=None)
+    img.save("" + str(i) + ".png")
+
