67ec336dcfe317a5ac21834936ba081be75be8bf
Andrey Sorokin <andrey.s.sorokin@raiffeisen.ru>
2019-08-11 13:19:44 +0300

    move generator to separated folder

--- generator/generator.py
+++ generator/generator.py
@@ -5,6 +5,7 @@ from os.path import isfile, join
 from PIL import Image, ImageDraw
 import numpy
 
+
 class ImageFactory(qrcode.image.base.BaseImage):
     """
     PIL image builder, default format is PNG.
@@ -44,14 +45,11 @@ class TextProvider:
 
     def get_text(self):
         length = random.randrange(20, 800)  # 20 -> 800
-        qr_string = self.text[self.read : length + self.read]
+        qr_string = self.text[self.read: length + self.read]
         self.read += length
         if len(qr_string) < length:
             self.read = 0
         return qr_string
-
-
-
 
 
 def create_qr(data, back_color="white"):
@@ -67,9 +65,11 @@ def create_qr(data, back_color="white"):
 
     return qr.make_image(image_factory=ImageFactory, fill_color="black", back_color=back_color)
 
+
 class Outputter:
     angle = 0
-    onlyfiles = [f for f in listdir("backgrounds") if isfile(join("backgrounds", f)) and f.endswith(('jpg', 'JPG'))]
+    onlyfiles = [f for f in listdir("backgrounds") if isfile(
+        join("backgrounds", f)) and f.endswith(('jpg', 'JPG'))]
 
     textProvider = TextProvider()
 
@@ -90,7 +90,8 @@ class Outputter:
             if background_name in self.avarage_colors.values():
                 average_color = self.avarage_colors[background_name]
             else:
-                average_color = background.resize((1, 1), Image.ANTIALIAS).getpixel((0, 0))
+                average_color = background.resize(
+                    (1, 1), Image.ANTIALIAS).getpixel((0, 0))
 
             background = background.convert('RGBA')
 
@@ -104,13 +105,15 @@ class Outputter:
                 dest = self.destination_point(background, img)
                 background.alpha_composite(img, dest=dest)
             except:
-                print("fail")
+                print("fail with " + background_name)
 
             file_name = self.number_string(i, bound)
             result = background.convert('RGB')
             masked = self.masked_image(background, img, dest)
-            result.save("./output/qr_" + file_name + ".image" + ".jpg", format="JPEG")
-            masked.save("./output/qr_" + file_name + ".mask" + ".jpg", format="JPEG")
+            result.save("./output/qr_" + file_name +
+                        ".image" + ".jpg", format="JPEG")
+            masked.save("./output/qr_" + file_name +
+                        ".mask" + ".jpg", format="JPEG")
             self.angle += 1
             if self.angle > 360:
                 self.angle = bound % 360
@@ -118,27 +121,35 @@ class Outputter:
     def perspective(self, img):
         width, height = img.size
         m = 0.1
-        xshift = abs(m) * width
-        print(xshift, width)
-        new_width = int(width - width / 7)
+        xshift = width * m
+        yshift = height * m
+        new_width = width - xshift
+        new_height = height - yshift
         coeffs = self.find_coeffs(
-            [(0, 0), (256, 0), (256, 256), (0, 256)],
-            [(0, 0), (256, 0), (new_width, height), (0, height)])
+            [(0, 0), (width, 0), (width, height), (0, height)],
+            [(0, 0), (width, yshift), (width, height), (0, height)])
         return img.transform((width, height),
-                                Image.PERSPECTIVE,
-                                coeffs,
-                                Image.BICUBIC)
+                             Image.PERSPECTIVE,
+                             coeffs,
+                             Image.BICUBIC)
 
     def find_coeffs(self, source_coords, target_coords):
         matrix = []
         for s, t in zip(source_coords, target_coords):
-            matrix.append([t[0], t[1], 1, 0, 0, 0, -s[0]*t[0], -s[0]*t[1]])
-            matrix.append([0, 0, 0, t[0], t[1], 1, -s[1]*t[0], -s[1]*t[1]])
+            matrix.append([
+                t[0], t[1], 1,
+                0, 0, 0,
+                -s[0]*t[0], -s[0]*t[1]
+            ])
+            matrix.append([
+                0, 0, 0,
+                t[0], t[1], 1,
+                -s[1]*t[0], -s[1]*t[1]
+            ])
         A = numpy.matrix(matrix, dtype=numpy.float)
         B = numpy.array(source_coords).reshape(8)
         res = numpy.dot(numpy.linalg.inv(A.T * A) * A.T, B)
         return numpy.array(res).reshape(8)
-
 
     def masked_image(self, result, qr, destination):
         mask = Image.new('RGBA', size=qr.size, color="red")
@@ -167,4 +178,3 @@ class Outputter:
 
 
 Outputter().start()
-
